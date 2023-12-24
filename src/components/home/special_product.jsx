b0341cc73ac013860a482bfd0cba8edb7ef0746a
ahmed zaky <aalsaeed@nadeer.com.sa>
2023-12-24 19:54:06 +0200

    updates

--- src/components/home/special_product.jsx
+++ src/components/home/special_product.jsx
@@ -12,60 +12,93 @@ import { Grid, Pagination } from 'swiper/modules';
 function SpecialProductSection () {
     let productData = [
         {
+            img1 : "/images/mobile.jpg",
+            img2 : "/images/mobile2.png",
+            img3 : "/images/mobile3.png",
+            type : "sony",
+            rate : 4.5,
+            header: 'hero session action camera with 8mb',
+            price : "$60.00",
+            oldPrice : "$75.00",
+            discount : "35%",
+            days : 878,
+            productPercentage : "35%"
+        },
+        {
             img1 : "/images/camera10.png",
             img2 : "/images/camera11.png",
             img3 : "/images/camera12.png",
             type : "havelis",
-            header: 'samsong galaxy note 10+ mobile phone sml...',
+            rate : 4,
+            header: 'samsong galaxy note 10+ mobile phone sml',
             price : "$60.00",
-            oldprice : "$75.00",
+            oldPrice : "$75.00",
+            discount : "40%",
+            days : 743,
+            productPercentage : "10%"
         },
+
         {
-            img1 : "/images/camera_classic.jpg",
-            img2 : "/images/camera.jpg",
-            type : "sony",
-            header: 'sed ut perspiciatis unde omnis',
-            price : "$100.00"
-        },
-        {
-            img1 : "/images/tab1.jpg",
-            img2 : "/images/tab.jpg",
+            img1 : "/images/speker1.jpg",
+            img2 : "/images/speker2.jpg",
+            img3 : "/images/speker3.jpg",
             type : "havelis",
-            header: 'vite magnes fuces laoreet porttitor',
-            oldPrice : "$55.22",
-            price : "$500.00",
-            discount : "-33%"
+            header: 'vite magnes fuces laoreet bluetooth',
+            price : "$90.00",
+            oldPrice : "$260.00",
+            days : 598,
+            productPercentage : "60%"
         },
         {
-            img1 : "/images/speaker.jpg",
+            img1 : "/images/smart_phone.jpg",
             img2 : "/images/black_mike.jpg",
+            img3 : "/images/watch.jpg",
             type : "bajaj",
             date : "11 june 2022",
             header: 'amera soman detram sandra elit cursus',
-            price : "$100.00",
+            oldPrice : "$260.00",
+            days : 598,
+            productPercentage : "10%"        
         },
         {
             img1 : "/images/headphone.jpg",
             img2 : "/images/yellow_headphone.jpg",
+            img3 : "/images/yellow_headphone.jpg",
             type : "havelis",
             header: 'a beautiful sunday morning renaissance',
-            price : "$100.00"
-        },
+            oldPrice : "$260.00",
+            days : 598,
+            productPercentage : "10%"          },
         {
-            img2 : "/images/smartWatch.jpg",
+            img2 : "/images/laptop.jpg",
             img1 : "/images/watch.jpg",
+            img3 : "/images/smart_phone.jpg",
             date : "11 june 2022",
             type : "sony",
             header: 'amera soman detram sandra elit cursus',
-            price : "$220.00"
+            oldPrice : "$260.00",
+            days : 598,
+            productPercentage : "20%" 
         },
         {
             img1 : "/images/black_mike.jpg",
             img2 : "/images/speaker.jpg",
+            img3 : "/images/black_mike.jpg",
             date : "11 june 2022",
             type : "bajaj",
             header: 'amera soman detram sandra elit cursus',
             price : "$220.00"
+        },
+        {
+            img1 : "/images/speker1.jpg",
+            img2 : "/images/speker2.jpg",
+            img3 : "/images/speker3.jpg",
+            type : "havelis",
+            header: 'vite magnes fuces laoreet bluetooth',
+            price : "$90.00",
+            oldPrice : "$260.00",
+            days : 598,
+            productPercentage : "60%"
         },
     ]
     
