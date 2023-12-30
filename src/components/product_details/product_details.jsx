f0e8124898b1a8b389a478673ceae6ddd997c13c
ahmed zaky <aalsaeed@nadeer.com.sa>
2023-12-30 17:52:32 +0200

    payment

--- src/components/product_details/product_details.jsx
+++ src/components/product_details/product_details.jsx
@@ -180,11 +180,26 @@ export default function ProductDetails() {
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis consectetur tempora velit inventore molestias, quae, expedita facere ipsa obcaecati </p>
                       </div>
                   </li>
-                  <li className="payment">
-                    <h5 className="text-capitalize">payment methods</h5>
-                    <div className="methods d-flex gap-2">
+                  <li className="payment py-4">
+                    <h5 className="text-capitalize text-center">payment methods</h5>
+                    <div className="methods d-flex gap-2 justify-content-center">
                       <div className="method">
-                        <img src="" alt="" />
+                        <img src="../../../public/images/payment/payment2.png" alt="" />
+                      </div>
+                      <div className="method">
+                        <img src="../../../public/images/payment/payment3.png" alt="" />
+                      </div>
+                      <div className="method">
+                        <img src="../../../public/images/payment/payment4.png" alt="" />
+                      </div>
+                      <div className="method">
+                        <img src="../../../public/images/payment/payment5.png" alt="" />
+                      </div>
+                      <div className="method">
+                        <img src="../../../public/images/payment/payment6.png" alt="" />
+                      </div>
+                      <div className="method">
+                        <img src="../../../public/images/payment/payment7.png" alt="" />
                       </div>
                     </div>
                   </li>
