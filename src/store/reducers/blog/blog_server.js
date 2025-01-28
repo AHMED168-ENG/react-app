26dd6fb492b69b7336560907d5583deffde5ac57
ahmed zaky <aalsaeed@nadeer.com.sa>
2025-01-28 10:33:31 +0200

    updates

--- src/store/reducers/blog/blog_server.js
+++ src/store/reducers/blog/blog_server.js
@@ -7,7 +7,7 @@ import axios from "axios";
 export const BlogServer = createAsyncThunk("blog/all" , async (arg , payload) => {
     const {rejectWithValue} = payload
     return await axios.get(`${process.env.REACT_APP_SERVER_URL}/blog?page=${arg.page}&limit=${arg.limit}`).then((res) => {
-        return res   
+        return res 
     }).catch(err => {
         return rejectWithValue(err)
     })  
